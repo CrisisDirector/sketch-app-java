401740f6205816be26a61a4a7696ab79f914f3e7
CrisisDirector <130583225+CrisisDirector@users.noreply.github.com>
2023-07-01 18:04:37 +0530

    Update SketchPad.java to make indentations consistent

--- src/sketchpad/SketchPad.java
+++ src/sketchpad/SketchPad.java
@@ -9,26 +9,26 @@ public class SketchPad {
     private final PaintingTool paint = new PaintingTool();
 
     public void initialLaunch() {
-      // the code here runs when the program is first started up
-      screen.setBackgroundColor(new Color(127, 255, 127));
+        // the code here runs when the program is first started up
+        screen.setBackgroundColor(new Color(127, 255, 127));
 
-      paint.setFillColor(Color.BLUE);
-      paint.drawCircle(0, 40, 20);
-      paint.drawCircle(0, 360, 20);
-      paint.drawCircle(600, 40, 20);
-      paint.drawCircle(600, 360, 20);
+        paint.setFillColor(Color.BLUE);
+        paint.drawCircle(0, 40, 20);
+        paint.drawCircle(0, 360, 20);
+        paint.drawCircle(600, 40, 20);
+        paint.drawCircle(600, 360, 20);
 
-      paint.setFillColor(Color.RED);
-      paint.drawCircle(40, 0, 20);
-      paint.drawCircle(40, 400, 20);
-      paint.drawCircle(560, 0, 20);
-      paint.drawCircle(560, 400, 20);
+        paint.setFillColor(Color.RED);
+        paint.drawCircle(40, 0, 20);
+        paint.drawCircle(40, 400, 20);
+        paint.drawCircle(560, 0, 20);
+        paint.drawCircle(560, 400, 20);
 
-      paint.setFillColor(Color.WHITE);
-      paint.drawRect(30, 30, 540, 340);
+        paint.setFillColor(Color.WHITE);
+        paint.drawRect(30, 30, 540, 340);
     }
 
     public void update() {
-      // the code here runs whenever the program's state changes
+        // the code here runs whenever the program's state changes
     }
 }
