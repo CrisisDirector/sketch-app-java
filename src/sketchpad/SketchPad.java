d233fe3024cb0d56c51352b52c120f6a61e67d86
CrisisDirector <130583225+CrisisDirector@users.noreply.github.com>
2023-07-04 09:57:22 +0530

    Update SketchPad.java with always on pen tool

--- src/sketchpad/SketchPad.java
+++ src/sketchpad/SketchPad.java
@@ -4,9 +4,14 @@ import buttons.*;
 import tools.*;
 
 public class SketchPad {
+    // set up screen, mouse tracker, and painting tools
     private final Screen screen = new Screen();
     private final MouseInfo mouseInfo = new MouseInfo();
     private final PaintingTool paint = new PaintingTool();
+
+    // Declare variables for the previous mouse coordinates
+    private int prevMouseX;
+    private int prevMouseY;
 
     public void initialLaunch() {
         // the code here runs when the program is first started up
@@ -35,34 +40,20 @@ public class SketchPad {
 
     public void update() {
         // the code here runs whenever the program's state changes
-        // currently, this causes a starburst of lines from the four corners to the mouse pointer's location
+        // currently, this implements an always-on pen tool: the mouse acts like a multi-coloured pen
+
+        // set the pen colour
+        paint.setLineColor(
+          Generator.randomInt(256),
+          Generator.randomInt(256),
+          Generator.randomInt(256)
+        );
         
-        paint.setLineColor(
-          Generator.randomInt(256),
-          Generator.randomInt(256),
-          Generator.randomInt(256)
-        );
-        paint.drawLine(30, 30, mouseInfo.getX(), mouseInfo.getY());
+        // draw a line from the current mouse location to the previous mouse location
+        paint.drawLine(prevMouseX, prevMouseY, mouseInfo.getX(), mouseInfo.getY());
 
-        paint.setLineColor(
-          Generator.randomInt(256),
-          Generator.randomInt(256),
-          Generator.randomInt(256)
-        );
-        paint.drawLine(570, 30, mouseInfo.getX(), mouseInfo.getY());
-
-        paint.setLineColor(
-          Generator.randomInt(256),
-          Generator.randomInt(256),
-          Generator.randomInt(256)
-        );
-        paint.drawLine(30, 370, mouseInfo.getX(), mouseInfo.getY());
-
-        paint.setLineColor(
-          Generator.randomInt(256),
-          Generator.randomInt(256),
-          Generator.randomInt(256)
-        );
-        paint.drawLine(570, 370, mouseInfo.getX(), mouseInfo.getY());
+        // Update the previous location values of the mouse
+        prevMouseX = mouseInfo.getX();
+        prevMouseY = mouseInfo.getY();
     }
 }
